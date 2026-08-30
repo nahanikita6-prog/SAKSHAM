e19f9dda06401baa2c19695f31fd01e643ccf7d2
Nikita Naha <nahanikita6@gmail.com>
2026-08-30 21:39:30 +0530

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://wzbjesifsrgqqbeossxa.supabase.co";
+
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_ojzjB_yf9ecptI9jARu3tw_FLLdTpQk";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY
+);
